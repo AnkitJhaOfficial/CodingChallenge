18c64767a84249fc06c2ea06bb1dabaffe390f56
Ankit Jha <ankit.jha@neosoftmail.com>
2023-07-28 20:25:18 +0530

    Added Solution for coding challenge maximum profit earned by buying and selling shares

--- src/MaximumProfitEarned.java
+++ src/MaximumProfitEarned.java
@@ -1,12 +1,14 @@
 public class MaximumProfitEarned {
     public static void main(String[] args) {
-
+        int[] stockPrices =  {1, 5, 2, 3, 7, 6, 4, 5};
+        int profitEarned = findMaximumProfitEarned(stockPrices);
+        System.out.println(profitEarned);
     }
 
     public static int findMaximumProfitEarned(int[] stockPrices){
         int profit=0;
 
-        for (int i = 0; i < stockPrices.length; i++) {
+        for (int i = 0; i < stockPrices.length-1; i++) {
             if(stockPrices[i]<stockPrices[i+1]){
                 profit=profit+stockPrices[i+1]-stockPrices[i];
             }
