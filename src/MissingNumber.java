dd094f67ec8fe135951c249304ca0155eb537c20
Ankit Jha <ankit.jha@neosoftmail.com>
2023-09-06 19:22:59 +0530

    Added solution for find missing number

---
+++ src/MissingNumber.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.OptionalInt;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] nums={9,6,4,2,3,5,7,0,1};
+        int missingNumberUsingMap = findMissingNumberUsingMap(nums);
+        System.out.println(missingNumberUsingMap);
+    }
+
+    public static int findMissingNumberUsingSet(int[] nums){
+        Set<Integer> integerSet = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        for (int i = 0; i <= nums.length; i++) {
+            if(!integerSet.contains(i)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int findMissingNumberUsingMap(int[] nums){
+        Map<Integer, Object> map = Arrays.stream(nums).boxed()
+                .collect(Collectors.toMap(integer -> integer, integer -> new Object()));
+        OptionalInt first = IntStream.range(0, nums.length).filter(value -> map.get(value) == null).findFirst();
+        if(first.isPresent()){
+            return first.getAsInt();
+        }
+        return -1;
+    }
+}
